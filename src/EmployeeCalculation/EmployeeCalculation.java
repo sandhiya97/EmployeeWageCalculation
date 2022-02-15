04d67cfd42199d7fc7a1179407a0cd21a28392d3
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-15 19:44:50 +0530

    Checked whether employee present or absent

--- src/EmployeeCalculation/EmployeeCalculation.java
+++ src/EmployeeCalculation/EmployeeCalculation.java
@@ -1,10 +1,20 @@
 package EmployeeCalculation;
-
 public class EmployeeCalculation {
+	
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-
+		System.out.println("Checking whether the Employee is present or absent");
+		int randomValue = (int)Math.round(Math.random());
+		System.out.println(randomValue);
+		if(randomValue == 1)
+		{
+			System.out.println("Employee is Present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
